fc81f8dd89b47275462028abfbce669cd8d20b4f
Insomnius <aliana.arib@gmail.com>
2017-12-22 17:33:31 +0700

    Menggunakan channel pada odddnumber-v2

--- oddnumber-v2.go
+++ oddnumber-v2.go
@@ -9,23 +9,28 @@ func main() {
 	start := time.Now()
 	fmt.Printf("Waktu Mulai: %02d:%02d:%02d \n", start.Hour(), start.Minute(), start.Second())
 
-	go loop()
+	diffparam := make(chan time.Duration)
+	finishparam := make(chan time.Time)
 
-	finish := time.Now()
+	go loop(start, diffparam, finishparam)
+
+	diff, finish := <-diffparam, <-finishparam
+
 	fmt.Printf("Waktu Selesai: %02d:%02d:%02d \n", finish.Hour(), finish.Minute(), finish.Second())
-	diff := finish.Sub(start)
 	fmt.Println("Perbedaan Waktu: ", diff)
-
-	var input string
-	fmt.Scanln(&input)
-	fmt.Println("done")
 }
 
-func loop() {
+func loop(start time.Time, diffparam chan time.Duration, finishparam chan time.Time) {
 	for i := 1; i <= 2000; i++ {
 		if i%2 == 0 {
 			continue
 		}
 		fmt.Println(i)
 	}
+
+	finishloop := time.Now()
+	diffloop := finishloop.Sub(start)
+
+	diffparam <- diffloop
+	finishparam <- finishloop
 }
